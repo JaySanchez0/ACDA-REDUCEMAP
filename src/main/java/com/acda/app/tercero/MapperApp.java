4b756e11949adf9e65fd72fa5e396038c49b7efa
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-03-24 12:49:48 -0500

    app

--- src/main/java/com/acda/app/tercero/MapperApp.java
+++ src/main/java/com/acda/app/tercero/MapperApp.java
@@ -1,9 +1,24 @@
 package com.acda.app.tercero;
 
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.json.JSONObject;
 
 public class MapperApp extends Mapper<Object, Text, Text, IntWritable>{
+	private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
 
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      for (String val: value.toString().split("\n")) {
+    	JSONObject obj = new JSONObject(val);
+    	int id = obj.getJSONObject("user").getInt("id");
+        word.set(String.valueOf(id));
+        context.write(word, one);
+      }
+    }
 }
--- src/main/java/com/acda/app/tercero/ReducerApp.java
+++ src/main/java/com/acda/app/tercero/ReducerApp.java
@@ -6,19 +6,15 @@ import java.util.StringTokenizer;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.json.JSONObject;
-import org.apache.hadoop.mapreduce.Mapper.Context;
 
 public class ReducerApp extends Reducer<Text,IntWritable,Text,IntWritable>{
-	private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
-
-    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-      for(String pal : value.toString().split("\n")) {
-    	JSONObject json = new JSONObject(pal);
-    	int id = json.getJSONObject("user").getInt("id");
-        word.set(String.valueOf(id));
-        context.write(word, one);
-      }
+	private IntWritable result = new IntWritable();
+    public void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException {
+    	 int sum = 0;
+         for (IntWritable val : values) {
+           sum += val.get();
+         }
+         result.set(sum);
+         context.write(key, result);
     }
 }
