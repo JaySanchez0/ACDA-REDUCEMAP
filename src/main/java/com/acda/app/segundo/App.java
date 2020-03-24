1072a5376f158189d5fca80f7f711783d4b10343
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-03-24 11:43:01 -0500

    app

--- src/main/java/com/acda/app/segundo/App.java
+++ src/main/java/com/acda/app/segundo/App.java
@@ -1,5 +1,28 @@
 package com.acda.app.segundo;
 
-public class App {
+import java.io.IOException;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class App {
+	public static void main(String[] args) throws IllegalArgumentException, IOException, ClassNotFoundException, InterruptedException {
+		Configuration conf = new Configuration();
+	    Job job = Job.getInstance(conf, "App Count Tweets");
+	    job.setJarByClass(App.class);
+	    job.setMapperClass(MapperApp.class);
+	    job.setCombinerClass(ReducerApp.class);
+	    job.setReducerClass(ReducerApp.class);
+	    job.setOutputKeyClass(Text.class);
+	    job.setOutputValueClass(IntWritable.class);
+	    FileInputFormat.addInputPath(job, new Path(args[0]));
+	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+	    System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
 }
---
+++ src/main/java/com/acda/app/segundo/MapperApp.java
@@ -0,0 +1,26 @@
+package com.acda.app.segundo;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class MapperApp extends Mapper<Object, Text, Text, IntWritable>{
+	private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+    	String data = value.toString().replace("{", " ").replace("}", " ").replace("[", " ").replace("]", " ").replace("null", "").replace(",", " ");
+      StringTokenizer itr = new StringTokenizer(data);
+      while (itr.hasMoreTokens()) {
+    	String pal = itr.nextToken();
+    	if(!pal.equals("an") && !pal.equals("a") && !pal.equals("and") && !pal.equals("are") && !pal.equals("or") && 
+    		!pal.equals("but") && !pal.equals("then") && !pal.equals("that")) {
+	        word.set(itr.nextToken());
+	        context.write(word, one);
+    	}
+      }
+    }
+}
